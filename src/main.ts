6f909c4d8d1204941fd1e60ef8130aaea5494afb
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2023-07-09 10:07:57 +0300

    Добавлен пропущенный type assertion

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { writeFile } from "fs";
 
 import { extractPostsOfGroup } from "./modules/extract-posts-of-group.js";
-import target from "./target.json";
+import target from "./target.json" assert { type: "json" };
 
 const groupsPosts = await extractPostsOfGroup(target, 5);
 
